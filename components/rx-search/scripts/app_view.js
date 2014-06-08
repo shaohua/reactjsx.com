84c48a3eb922c2e6de28890707e70a3d34af5261
Shaohua Zhou <sh.zhou@gmail.com>
2014-06-07 21:40:17 -0700

    WIP for firebase

--- components/rx-search/scripts/app_view.js
+++ components/rx-search/scripts/app_view.js
@@ -2,9 +2,43 @@
 var _ = require('underscore'),
   React = require('react');
 
+function getStoreState() {
+  return {
+    items: []
+  };
+}
+
 var AppView = React.createClass({
+  getInitialState: function() {
+    this.items = [];
+    this.firebaseRef = new Firebase("//reactjsx.firebaseio.com/components");
+
+    return getStoreState();
+  },
+
+  componentWillMount: function() {
+    this.firebaseRef.on("child_added", function(dataSnapshot) {
+      console.log('child_added', arguments);
+      this.items.push(dataSnapshot.val());
+      this.setState({
+        items: this.items
+      });
+    }.bind(this));
+  },
+
+  componentDidMount: function() {
+  },
+
+  componentWillUnmount: function() {
+    this.firebaseRef.off();
+  },
+
+
   onClick: function(){
     console.log('onClick');
+    this.firebaseRef.push({
+      text: 'test'
+    });
   },
 
   render: function() {
