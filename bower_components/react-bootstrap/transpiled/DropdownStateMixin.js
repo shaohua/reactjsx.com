08459d331f1d65e9718dfd6b9cf0a25ffa9e9416
Shaohua <sh.zhou@gmail.com>
2014-06-17 19:45:37 -0700

    Adding bower_components and dist directory

---
+++ bower_components/react-bootstrap/transpiled/DropdownStateMixin.js
@@ -0,0 +1,77 @@
+define(
+  ["./react-es6","exports"],
+  function(__dependency1__, __exports__) {
+    "use strict";
+    var React = __dependency1__["default"];
+
+    /**
+     * Checks whether a node is within
+     * a root nodes tree
+     *
+     * @param {DOMElement} node
+     * @param {DOMElement} root
+     * @returns {boolean}
+     */
+    function isNodeInRoot(node, root) {
+      while (node) {
+        if (node === root) {
+          return true;
+        }
+        node = node.parentNode;
+      }
+
+      return false;
+    }
+
+    var DropdownStateMixin = {
+      getInitialState: function () {
+        return {
+          open: false
+        };
+      },
+
+      setDropdownState: function (newState, onStateChangeComplete) {
+        if (newState) {
+          this.bindRootCloseHandlers();
+        } else {
+          this.unbindRootCloseHandlers();
+        }
+
+        this.setState({
+          open: newState
+        }, onStateChangeComplete);
+      },
+
+      handleKeyUp: function (e) {
+        if (e.keyCode === 27) {
+          this.setDropdownState(false);
+        }
+      },
+
+      handleDocumentClick: function (e) {
+        // If the click originated from within this component
+        // don't do anything.
+        if (isNodeInRoot(e.target, this.getDOMNode())) {
+          return;
+        }
+
+        this.setDropdownState(false);
+      },
+
+      bindRootCloseHandlers: function () {
+        document.addEventListener('click', this.handleDocumentClick);
+        document.addEventListener('keyup', this.handleKeyUp);
+      },
+
+      unbindRootCloseHandlers: function () {
+        document.removeEventListener('click', this.handleDocumentClick);
+        document.removeEventListener('keyup', this.handleKeyUp);
+      },
+
+      componentWillUnmount: function () {
+        this.unbindRootCloseHandlers();
+      }
+    };
+
+    __exports__["default"] = DropdownStateMixin;
+  });
